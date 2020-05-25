b276cb311381fb4a3c0bf5977e731d89647f8417
bastijnkostense <bastijnkostense@hotmail.nl>
2020-05-25 21:46:30 +0200

    Add radio buttons for video clarity and agreeing of worker

--- index.js
+++ index.js
@@ -3,19 +3,41 @@ startTime = getRandomInt(0, 100)
 endTime = startTime + 10
 
 function process(){
+    // Declare values and find them in the for-loops
     value = null
+    clarityValue = null
+    agreeValue = null
 
-    elements = document.getElementsByName("sentiment")
+    elements = document.getElementsByName("sentiment");
     for(i = 0; i < elements.length; i++) {
         if(elements[i].checked) {
             value = elements[i].value
         }
     }
 
+    clarityElements = document.getElementsByName("clarity");
+    for(i = 0; i < clarityElements.length; i++) {
+        if(clarityElements[i].checked) {
+            clarityValue = clarityElements[i].value
+        }
+    }
+
+    agreeElements = document.getElementsByName("content");
+    for(i = 0; i < agreeElements.length; i++) {
+        if(agreeElements[i].checked) {
+            agreeValue = agreeElements[i].value
+        }
+    }
+
+    // Alert if any of the values are null, submit values otherwise
     if (value == null) {
         alert("You did not choose a sentiment yet.");
+    } else if (clarityValue == null) {
+        alert("You did not choose whether the video was clear yet.");
+    } else if (agreeValue == null) {
+        alert("You did not choose whether you agreed with the video yet.");
     } else {
-        alert("You've chosen: " + value + "\n\nVideo id: " + videoId + "\nStart time: " + startTime + "\nEnd time: " + endTime);
+        alert("Sentiment: " + value + "\nClear: " + clarityValue + "\nAgree:" + agreeValue + "\n\nVideo id: " + videoId + "\nStart time: " + startTime + "\nEnd time: " + endTime);
         location.reload();
     }
 }
