048f8469102b92eb894a7e55db8ab87ea2f961f9
Joost Commandeur <joostcommandeur@outlook.com>
2020-05-25 21:30:00 +0200

    html radio buttens added, .js added

--- __init__.py
+++ __init__.py
@@ -2,4 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from app import routes
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
--- index.js
+++ index.js
@@ -3,19 +3,36 @@ startTime = getRandomInt(0, 100)
 endTime = startTime + 10
 
 function process(){
-    value = null
+    valueSentiment = null
+    valueOpinion = null
+    valueClear = null
 
     elements = document.getElementsByName("sentiment")
     for(i = 0; i < elements.length; i++) {
         if(elements[i].checked) {
-            value = elements[i].value
+            valueSentiment = elements[i].value
         }
     }
 
-    if (value == null) {
+    elements = document.getElementsByName("opinion")
+    for(i = 0; i < elements.length; i++) {
+        if(elements[i].checked) {
+            valueOpinion = elements[i].value
+        }
+    }
+
+    elements = document.getElementsByName("clear")
+    for(i = 0; i < elements.length; i++) {
+        if(elements[i].checked) {
+            valueClear = elements[i].value
+        }
+    }
+
+
+    if (valueSentiment== null) {
         alert("You did not choose a sentiment yet.");
     } else {
-        alert("You've chosen: " + value + "\n\nVideo id: " + videoId + "\nStart time: " + startTime + "\nEnd time: " + endTime);
+        alert("You've chosen sentiment: " + valueSentiment + "\n\nVideo id: " + videoId + "\nStart time: " + startTime + "\nEnd time: " + endTime);
         location.reload();
     }
 }
