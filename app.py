3380decb952e006356a9c59ec2084e2d466f0189
Ashay <a.s.s.somai@student.tudelft.nl>
2020-05-26 12:46:32 +0200

    Give random entries from csv file to frontend, change result directory for assessments

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, make_response, json
 from flask import redirect, request, jsonify, url_for
 import json, csv, os
-import pandas as pd
+from random import randint
 
 import os
 
@@ -14,11 +14,13 @@ def index():
     return render_template('layouts/index.html',
                            title=title)
 
+
 @app.route('/positive.html', methods=['GET'])
 def positivePage():
     title = 'Create the input'
     return render_template('layouts/positive.html',
                            title=title)
+
 
 @app.route('/negative.html', methods=['GET'])
 def negativePage():
@@ -26,29 +28,36 @@ def negativePage():
     return render_template('layouts/negative.html',
                            title=title)
 
+
 @app.route('/assessment.html', methods=['GET'])
 def assessmentPage():
     title = 'Create the input'
     return render_template('layouts/assessment.html',
                            title=title)
 
+
 @app.route('/postmethod', methods = ['POST'])
 def post_javascript_data():
     jsdata = request.form['video_data']
     jsdata = json.loads(jsdata)
-    with open("assessment-" + jsdata["videoid"] + ".csv", mode="a+", newline='') as csvfile:
+    with open("results/assessment-" + jsdata["videoid"] + ".csv", mode="a+", newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow([jsdata["videoid"], jsdata["starttime"], jsdata["endtime"], jsdata["value"], jsdata["clarity"], jsdata["agree"]])
         csvfile.close()
 
     return "", 200
 
+
 @app.route('/getmethod', methods= ['GET'])
 def get_javascript_data():
-    df = pd.read_csv('data/res.csv')
-    print(df)
+    videos = list()
+    with open('data/res.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile, ['videoId', 'startTime', 'endTime'])
+        for row in reader:
+            videos.append(row)
 
-    return jsonify(videoId="mii6NydPiqI", startTime=10, endTime=20)
+    random = randint(0, len(videos) - 1)
+    return jsonify(videoId=videos[random]['videoId'], startTime=videos[random]['startTime'], endTime=videos[random]['endTime'])
 
 
 if __name__ == '__main__':
