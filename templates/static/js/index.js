dd917b2b9edda53ab378af48b428a4605144fad4
bastijnkostense <bastijnkostense@hotmail.nl>
2020-06-02 13:53:43 +0200

    Add training options to positive example

--- templates/static/js/index.js
+++ templates/static/js/index.js
@@ -96,3 +96,18 @@ function negNext() {
     window.location.replace("assessment.html");
 }
 
+function trainingLoadAgree() {
+    document.getElementById("agree-intro").style.display = 'block';
+}
+
+function trainingLoadClear() {
+    document.getElementById("clear-intro").style.display = 'block';
+}
+
+function trainingDone() {
+document.getElementById("next").style.display = 'block';
+}
+
+function wrongPositive() {
+    alert("Remember, this is a positive example!");
+}
